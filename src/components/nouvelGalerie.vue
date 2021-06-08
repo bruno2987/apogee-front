07791567ffae65e67ed5c4e6d68afeb7b29ff396
unknown <bruno.dubief@hotmail.fr>
2021-06-08 09:43:04 +0200

    dropDrag

--- src/components/nouvelGalerie.vue
+++ src/components/nouvelGalerie.vue
@@ -1,35 +1,17 @@
 <template>
     <div>
-<vue-upload-multiple-image 
-    @upload-success="uploadImageSuccess"
-    @edit-image="editImage"
-    @limit-exceeded="limitExceeded"
-    @before-remove="beforeRemove"
-    id-upload="myIdUpload"
-    id-edit="myIdEdit"
-    :max-image=20
-    primary-text="Default"
-    browse-text="Browse picture(s)"
-    drag-text="Drag pictures"
-    mark-is-primary-text="Set as default"
-    popup-text="This image will be displayed as default"
-    :multiple=true
-    :show-edit=true
-    :show-delete=true
-    :show-add=true
- >
- </vue-upload-multiple-image>
+        <UploadImages @change="handleImages"/>
  <h1>{{test}}</h1>
     </div>
 </template>
 
 <script>
-import VueUploadMultipleImage from 'vue-upload-multiple-image';
+import UploadImages from "vue-upload-drop-images";
 
 export default {
     name: 'nouvelGalerie',
     components: {
-        VueUploadMultipleImage
+        UploadImages,
     },
     data() {
         return {
@@ -37,9 +19,10 @@ export default {
         }
     },
     methods: {
-        uploadImageSuccess(formData) {
-            this.test= 'Message TEST'
-            this.http.post('http://localhost:3000/admin/galerie/recordGalerie',formData)
+        handleImages(files) {
+            this.test= 'Mess TEST'
+            console.log(files)
+            this.http.post('http://localhost:3000/admin/galerie/recordGalerie',{files : files})
                 .then((response) => {
                     console.log(response)
                 })
